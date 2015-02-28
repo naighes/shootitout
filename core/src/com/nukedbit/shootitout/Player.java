c9e1e40a72071a88c871c9fa65f79284089615ec
Nicola Baldi <nic.baldi@gmail.com>
2015-02-28 18:01:59 +0100

    removing unused namespace.

--- core/src/com/nukedbit/shootitout/Player.java
+++ core/src/com/nukedbit/shootitout/Player.java
@@ -3,7 +3,6 @@ package com.nukedbit.shootitout;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class Player extends SceneComponentBase {
     private Texture playerTexture;
