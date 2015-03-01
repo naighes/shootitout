9adc7c216533b0325098c878feae6b0f7859c4d8
Nicola Baldi <nic.baldi@gmail.com>
2015-03-01 08:50:29 +0100

    equality member for KeyPressed.

--- core/src/com/nukedbit/shootitout/KeyPressed.java
+++ core/src/com/nukedbit/shootitout/KeyPressed.java
@@ -1,7 +1,21 @@
 package com.nukedbit.shootitout;
 
 public class KeyPressed {
-    public KeyPressed(int key) {
+    private final int key;
 
+    public KeyPressed(int key) {
+        this.key = key;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        boolean result = false;
+
+        if (other instanceof KeyPressed) {
+            KeyPressed that = (KeyPressed) other;
+            result = this.key == that.key;
+        }
+
+        return result;
     }
 }
