da7548e26cfbcaff4cbe1f2d55d330e5a6f89daa
Sebastian Faltoni <sebastian.faltoni@gmail.com>
2015-03-09 23:53:19 +0100

    moved speedFactor to its own

--- core/src/com/nukedbit/shootitout/components/Background.java
+++ core/src/com/nukedbit/shootitout/components/Background.java
@@ -6,9 +6,7 @@ import com.nukedbit.framework.components.GameBase;
 import com.nukedbit.framework.physics.WorldObject;
 
 public class Background extends com.nukedbit.framework.components.Sprite implements WorldObject {
-    private float speed = 10f;
-
-    private Vector2 position;
+    private final float speedFactor = 0.002f;
 
     protected Background(GameBase game, String texturePath) {
         super(game, texturePath);
@@ -21,18 +19,25 @@ public class Background extends com.nukedbit.framework.components.Sprite impleme
         final int viewPortWidth = this.getGame().getViewPort().getWidth();
         final int viewportHeight = this.getGame().getViewPort().getHeight();
 
-        this.position = new Vector2(viewPortWidth /2 * -1f,
-                                    viewportHeight /2 * -1f);
+        setTextureWrap();
 
+        setSpriteBounds(viewPortWidth, viewportHeight);
+    }
+
+    private void setTextureWrap() {
         final Texture texture = this.innerSprite.getTexture();
         texture.setWrap(Texture.TextureWrap.Repeat, Texture.TextureWrap.Repeat);
+    }
 
-        this.innerSprite.setPosition(this.position.x, this.position.y);
+    private void setSpriteBounds(int viewPortWidth, int viewportHeight) {
+        final Texture texture = this.innerSprite.getTexture();
+        Vector2 position = new Vector2(viewPortWidth /2 * -1f,
+                viewportHeight /2 * -1f);
+        this.innerSprite.setSize(texture.getWidth(), texture.getHeight());
+        this.innerSprite.setPosition(position.x, position.y);
         this.innerSprite.setSize(Math.abs(texture.getWidth()), Math.abs(texture.getHeight()));
         this.innerSprite.setOrigin(texture.getWidth() / 2, texture.getHeight() / 2);
         this.innerSprite.setRegion(0,0, viewPortWidth, viewportHeight);
-
-        this.innerSprite.setSize(texture.getWidth(), texture.getHeight() );
     }
 
     float scrollTimer= 0;
@@ -40,7 +45,7 @@ public class Background extends com.nukedbit.framework.components.Sprite impleme
     public void update(float dt) {
         super.update(dt);
 
-        scrollTimer -= dt * 0.002f;
+        scrollTimer -= dt * speedFactor;
         if(scrollTimer>1.0f)
             scrollTimer = 0.0f;
 
